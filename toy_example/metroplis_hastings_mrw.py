894bd53020aa8828b8793f488225748b57290ba7
fluffems <mkl@mikiaholding.dk>
2025-05-13 00:55:49 +0200

    made MHRW and kl for it

--- toy_example/metroplis_hastings_mrw.py
+++ toy_example/metroplis_hastings_mrw.py
@@ -1,42 +1,64 @@
-from GPED import alpha, beta
-from toydata import theta_init, target
-from GPED import algo2D
+from toydata import theta_init, target, algo2D
 from statistics import weight_kl
 import torch
 import matplotlib.pyplot as plt
+from torch.distributions.multivariate_normal import MultivariateNormal
+from torch.distributions.kl import kl_divergence
 
-def MHRW(algo2D, theta_init, target, T=100):
+def MHRW(algo2D, theta_init, T=10000):
     theta = theta_init.detach().clone().requires_grad_(True)
-    accepted_proposals = [None]*T
-    for t in T:
-        g = torch.rand()
-        proposal = algo2D.log_likelihood(theta+g) + algo2D.log_prior(theta+g)
-        current = target.log_prob(theta) + algo2D.log_prior(theta)
+    chain = [None]*T
+    eta = 1
+    for t in range(T):
+        thetaprime = theta.detach() + eta*torch.randn_like(theta)
+        #algo2D.log_joint(theta+g).item() would yield the same
+        log_proposal = (algo2D.log_likelihood(thetaprime) + algo2D.log_prior(thetaprime))
+        log_current = (algo2D.log_likelihood(theta) + algo2D.log_prior(theta))
         
-        A = torch.min(1, proposal - current)
-        u = torch.rand()
-        if torch.min(1,A) <= u:
-            accepted_proposals[t] = u
-            print("chose u")
-        else:
-            theta = proposal
-            accepted_proposals[t] = proposal
-            print("chose propsal")
-    return torch.stack(accepted_proposals)
+        A = torch.min(torch.tensor([0]), log_proposal - log_current)
+        u = torch.log(torch.rand(1))
+        if u <= A:
+            theta = thetaprime
+            # print("chose propsal")
+        chain[t] = theta.detach().clone()
+        
+
+    return torch.stack(chain)
 
 
-
-RW_samples = MHRW(algo2D=algo2D, theta_init=theta_init, T=1060)
-
-
-
-    
-# sample_sz = w_MALA.shape[0]
-KL_RW, KL_baseline, sample_true = weight_kl(W_samples=RW_samples, target=target)
+T = 1060
+RW_samples = MHRW(algo2D=algo2D, theta_init=theta_init, T=T)
 
 
-t = torch.arange(0,len(RW_samples))
-plt.axhline(KL_baseline, label="Random Sample from Analytical posterior", linestyle="--")
-plt.plot(t, KL_RW, label="MALA KL", color = "green")
+#kl_divergences at various time T
+def kl_RW(RW_samples, target):
+    offset = 5
+    x = torch.arange(offset,len(RW_samples))
+    kl_rw = torch.zeros(len(RW_samples)-offset)    
+    for i, id in enumerate(x):
+        w_s = RW_samples[0:id]
+        W_est = torch.mean(w_s, axis=0)        
+        W_shat = torch.cov(w_s.T) + torch.eye(2) * 1e-6
+        W_guess = MultivariateNormal(W_est, W_shat)
+        kl_rw[i] = kl_divergence(W_guess, target).item()   
+    return kl_rw
+
+
+#sample baseline
+def kl_baseline(target):
+    target_samples = target.rsample((RW_samples.shape[0],))
+    mhat_true = torch.mean(target_samples,axis=0)
+    shat_true = torch.cov(target_samples.T)
+    sample_true = MultivariateNormal(mhat_true, shat_true)
+    KL_true = kl_divergence(sample_true, target)
+    return KL_true
+
+kl_rw = kl_RW(RW_samples, target)
+kl_target = kl_baseline(target)
+
+t = torch.arange(5,len(RW_samples))
+plt.axhline(kl_target, label="Random Sample from Analytical posterior", linestyle="--")
+plt.plot(t, kl_rw, label="MHRW KL", color = "green")
 plt.tight_layout()
-plt.show(block=False)
+# plt.show(block=False)
+print("kek")
